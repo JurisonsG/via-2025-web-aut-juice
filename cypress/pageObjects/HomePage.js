2b277ad1f509a628d2c2da6eb2a0da60c05fb3e7
JurisonsG <151744605+JurisonsG@users.noreply.github.com>
2025-04-25 14:21:13 +0300

    Summary
    JSHOP

--- cypress/pageObjects/HomePage.js
+++ cypress/pageObjects/HomePage.js
@@ -12,4 +12,46 @@ export class HomePage extends BasePage {
   static get meWantItButton() {
     return cy.get("[aria-label='dismiss cookie message']");
   }
+
+  static get accountButton() {
+    return cy.get("button#navbarAccount")
+  }
+  static get loginButton() {
+    return cy.get("button#navbarLoginButton")
+  }
+
+  static get userProfileButton() {
+    return cy.get("button[aria-label='Go to user profile']");
+  }
+
+  static get searchIcon() {
+    return cy.get("#searchQuery");
+
+  }
+  static get searchField() {
+    return cy.get("#searchQuery input");
+
+  }
+
+  static get productBox() {
+    return cy.get("div.mdc-card");
+  }
+  static get productInfo() {
+    return cy.get("app-product-details");
+  }
+  static get closeButton() {
+    return cy.get("button[aria-label='Close Dialog']");
+  }
+
+  static get reviewsButton() {
+    return cy.get("[aria-label='Expand for Reviews']");
+  }
+
+  static get reviewField() {
+    return cy.get("[aria-label='Text field to review a product']");
+  }
+  static get submitReviewButton() {
+    return cy.get("#submitButton");
+  } 
+
 }
